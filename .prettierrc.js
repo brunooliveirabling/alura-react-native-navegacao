9fa75a83376bdbdcff38609960e060260d286e12
Bruno Santana de Oliveira <bruno.oliveira@Brunos-MacBook-Pro.local>
2023-07-11 13:55:09 -0300

    criadas telas Home e MelhoresProdutores / criada navegacao por tab no App.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    printWidth: 120,
 };
--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './src/telas/Home';
-import Cesta from './src/telas/Cesta';
+import MelhoresProdutores from './src/telas/MelhoresProdutores';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const produtores = useProdutores(false);
-
-  return <SafeAreaView style={{ flex: 1 }}>
-    <StatusBar />
-    <Home melhoresProdutores={false} />
-    {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    } */}
-  </SafeAreaView>
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar />
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="Home" component={Home} />
+                    <Tab.Screen name="MelhoresProdutores" component={MelhoresProdutores} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
 }
