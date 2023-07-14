46811959f4a58767d0ab13893473b5babf711f43
Bruno Santana de Oliveira <bruno.oliveira@Brunos-MacBook-Pro.local>
2023-07-14 09:24:50 -0300

    melhorado estilos da tela Produtor

--- src/componentes/Topo.js
+++ src/componentes/Topo.js
@@ -11,43 +11,44 @@ const largura = Dimensions.get('screen').width;
 const ALTURA_PADRAO = 270;
 
 export default function Topo({ titulo, imagem = topo, altura = ALTURA_PADRAO }) {
-  const estilos = funcaoEstilos(altura);
-  return <>
-    <Image source={imagem} style={estilos.topo} />
-    <Gradiente width={largura} height={130 / 360 * largura} style={estilos.gradiente}/>
-    <Texto style={estilos.titulo}>{ titulo }</Texto>
-    <TouchableOpacity 
-      onPress={() => {}}
-      style={estilos.botaoVoltar}>
-      <VoltarSVG color='white' style={estilos.voltar} />
-    </TouchableOpacity>
-  </>
+    const estilos = funcaoEstilos(altura);
+    return (
+        <>
+            <Image source={imagem} style={estilos.topo} />
+            <Gradiente width={largura} height={(130 / 360) * largura} style={estilos.gradiente} />
+            <Texto style={estilos.titulo}>{titulo}</Texto>
+            <TouchableOpacity onPress={() => {}} style={estilos.botaoVoltar}>
+                <VoltarSVG color="white" style={estilos.voltar} />
+            </TouchableOpacity>
+        </>
+    );
 }
 
-const funcaoEstilos = (altura) => StyleSheet.create({
-  topo: {
-    width: "100%",
-    height: altura,
-  },
-  gradiente: {
-    position: 'absolute',
-  },
-  titulo: {
-    width: "100%",
-    position: "absolute",
-    textAlign: "center",
-    fontSize: 16,
-    lineHeight: 26,
-    color: "white",
-    fontWeight: "bold",
-    padding: 16,
-  },
-  botaoVoltar: {
-    position: 'absolute',
-    padding: 17,
-  },
-  voltar: {
-    width: 24,
-    height: 24,
-  },
-});
+const funcaoEstilos = altura =>
+    StyleSheet.create({
+        topo: {
+            width: '100%',
+            height: altura,
+        },
+        gradiente: {
+            position: 'absolute',
+        },
+        titulo: {
+            width: '100%',
+            position: 'absolute',
+            textAlign: 'center',
+            fontSize: 16,
+            lineHeight: 26,
+            color: 'white',
+            fontWeight: 'bold',
+            padding: 16,
+        },
+        botaoVoltar: {
+            position: 'absolute',
+            padding: 17,
+        },
+        voltar: {
+            width: 24,
+            height: 24,
+        },
+    });
--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { FlatList, View, Text, Image, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View, Image, Text } from 'react-native';
 import Cesta from './componentes/Cesta';
+import Topo from '../../componentes/Topo';
+import useTextos from '../../hooks/useTextos';
+import topo from '../../assets/produtores/topo.png';
 
 export default function Produtor() {
     const route = useRoute();
+    const { tituloProdutor, tituloCestas } = useTextos();
     const { nome, imagem, distancia, estrelas, cestas } = route.params;
 
     function HeaderProdutor() {
         return (
             <>
-                <Text>Olá! Eu sou o {nome}</Text>
-                <Image source={imagem} />
-                <Text>Estou a {distancia}m</Text>
-                <Text>E tenho {estrelas} estrelas</Text>
+                <Topo titulo={tituloProdutor} imagem={topo} altura={158} />
+                <View style={estilos.conteudo}>
+                    <View style={estilos.logo}>
+                        <Image source={imagem} style={estilos.produtorImage} />
+                        <View>
+                            <Text style={estilos.produtor}>{nome}</Text>
+                            <Text style={estilos.miniInfos}>
+                                Distância {distancia}m Estrelas: {estrelas}
+                            </Text>
+                        </View>
+                    </View>
+                    <Text style={estilos.cestas}>{tituloCestas}</Text>
+                </View>
             </>
         );
     }
 
-    return <FlatList data={cestas} renderItem={({ item }) => <Cesta {...item} produtor={{ nome, imagem }} />} />;
+    return (
+        <FlatList
+            data={cestas}
+            renderItem={({ item }) => <Cesta {...item} produtor={{ nome, imagem }} />}
+            style={estilos.lista}
+            ListHeaderComponent={HeaderProdutor}
+        />
+    );
 }
 
 const estilos = StyleSheet.create({
@@ -52,5 +72,10 @@ const estilos = StyleSheet.create({
         lineHeight: 32,
         fontWeight: 'bold',
         marginTop: 32,
+    },
+    miniInfos: {
+        fontSize: 10,
+        color: '#555555',
+        marginLeft: 16,
     },
 });
